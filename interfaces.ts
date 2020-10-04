e2264fde589558a9d79bc6b73f0bd2dee1da9a89
AnthonyTB <anthonylol@protonmail.com>
2020-10-03 20:45:18 -0700

    got data setup and returns fine just have to work on if the collection is paginated

---
+++ interfaces.ts
@@ -0,0 +1,8 @@
+export interface IItemObject {
+  ItemName: string;
+  ItemImg: string;
+  ItemImgAlt: string;
+  ItemLink: string;
+  ItemPrice: number;
+  ItemSoldOut: boolean;
+}
--- scrape.ts
+++ scrape.ts
@@ -1,5 +1,6 @@
 import cheerio from "cheerio";
 import express from "express";
+import { IItemObject } from "./interfaces";
 import fetch from "node-fetch";
 
 const app = express();
@@ -11,22 +12,57 @@ app.use(function errorHandler(error: any, req: any, res: any, next: any) {
 
 app.route("/query/:name").get(async (req, res) => {
   const { name } = req.params;
-  const url = `https://metathreads.com/collections/${name}`;
-
-  const response = await fetch(url);
+  let pageCount: number = 1;
+  const url: string = `https://metathreads.com/collections/${name}?page=${pageCount}`;
+  let response = await fetch(url);
   const html = await response.text();
   const itemArray: any = [];
   const $ = cheerio.load(html);
-  $(".ProductItem .ProductItem__Wrapper a").each((i: number, el) => {
+
+  const isPaginated =
+    $(".Pagination").has(".Pagination__Nav").length > 0 ? true : false;
+
+  $(".ProductItem__Wrapper").each(async (i: number, el) => {
+    const ItemName = $(el).find(".ProductItem__Title a").text();
+    const ItemImg = $(el)
+      .find(".ProductItem__Image")
+      .attr("data-src")
+      .replace(/\s*\{.*?\}\s*/g, "600");
+    const ItemImgAlt = $(el).find(".ProductItem__Image").attr("alt");
+    const ItemLink = `https://metathreads.com${$(el)
+      .find(".ProductItem__Title a")
+      .attr("href")}`;
+    const ItemPrice = Number(
+      $(el).find(".ProductItem__PriceList span").first().text().substring(2)
+    );
+    const ItemSoldOut =
+      $(el)
+        .find(".ProductItem__LabelList .ProductItem__Label")
+        .text()
+        .toLowerCase() === "sold out"
+        ? true
+        : false;
+
+    const ResponseObject: IItemObject = {
+      ItemName,
+      ItemImg,
+      ItemImgAlt,
+      ItemLink,
+      ItemPrice,
+      ItemSoldOut,
+    };
+    console.log(ResponseObject, pageCount);
     itemArray.push({
-      [i]: { Link: `https://metathreads.com${$(el).attr("href")}` },
+      [i]: ResponseObject,
     });
   });
-  $(".ProductItem .ProductItem__Wrapper div h2 a").each((i: number, el) => {
-    itemArray.push({
-      [i]: { Heading: $(el).text() },
-    });
-  });
+
+  if (isPaginated) {
+    ++pageCount;
+    console.log(pageCount);
+    response = await fetch(url);
+  }
+
   if (itemArray.length) {
     res.status(200).json(itemArray).end();
   } else {
